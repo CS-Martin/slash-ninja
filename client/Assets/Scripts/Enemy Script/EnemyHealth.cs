02d36f4d80daee984fd9cb069f75d70d27dcb255
Martin <meatole.cs@gmail.com>
2023-09-24 16:43:57 +0800

    Feat: Score system and font changes

--- client/Assets/Scripts/Enemy Script/EnemyHealth.cs
+++ client/Assets/Scripts/Enemy Script/EnemyHealth.cs
@@ -7,20 +7,20 @@ public class EnemyHealth : MonoBehaviour
     public Animator myanimator;
     public int maxHealth = 2;
     public int currentHealth;
-    
-    void Start()
+
+    private void Start()
     {
         currentHealth = maxHealth;
     }
 
-    
-    public void takeDamage(int amount){
-        //hurt animation
+    public void takeDamage(int amount)
+    {
         currentHealth -= amount;
-        if(currentHealth <= 0){
-            //death animation
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Enemy died!");
+            PlayerScript.AddScore(10);
             Destroy(gameObject);
         }
-
     }
 }
