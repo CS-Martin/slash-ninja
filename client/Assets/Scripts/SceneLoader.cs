5317ad473bc5f97a1b814fed6f16ab401f7f5756
Martin <meatole.cs@gmail.com>
2023-09-24 17:17:04 +0800

    Add: Prompts user to enter name upon clicking play button

--- client/Assets/Scripts/SceneLoader.cs
+++ client/Assets/Scripts/SceneLoader.cs
@@ -8,4 +8,9 @@ public class SceneLoader : MonoBehaviour
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     } 
+
+    public void QuitGame() {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
 }
